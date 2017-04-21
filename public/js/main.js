fca66a16e84a356c78eaa119e3721bca8bfaa3a7
Bryant Oblad <bryant.oblad@gmail.com>
2017-04-21 16:57:35 -0600

    adding in mobile version

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
 ;(function ($){
     $(function() {
+
+
+	$('#mobile-icon').click(function() {
+		$(this).addClass('shrink')
+		window.setTimeout(function() {
+			$('.header__mobile-wrap').addClass('pull-out');
+		}, 400)
+	})
 	
+	$('#mobile-close').click(function() {
+		$('.header__mobile-wrap').removeClass('pull-out');
+		window.setTimeout(function() {
+			$('#mobile-icon').removeClass('shrink')
+		}, 400)
+	})
+
 	$($('.report__row')[$('.report__row').length - 1]).css({marginBottom: 0})
 	if (document.getElementById('report-graph')) {
 		var width = document.getElementById('report-graph').offsetWidth;
