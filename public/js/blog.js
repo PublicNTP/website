639ae4dc571560a049ef259f0af3dcc4c30bea19
boblad <bryant.oblad@gmail.com>
2017-05-05 10:36:50 -0600

    adding in year timeline on blog

--- public/js/blog.js
+++ public/js/blog.js
@@ -11,5 +11,20 @@
 			$('.blog__search-item-wrap').removeClass('blog__search-input-hide');
 		})
 
+		$('.time__year').click(function() {
+			var time_wrap = $(this).siblings('.time__wrap');
+			if ($(time_wrap[0]).hasClass('time__closed')) {
+				$($(this).find('.time__icon')[0]).addClass('unrotate');
+				time_wrap.each(function(index) {
+					$(time_wrap[index]).removeClass('time__closed')
+				});
+			} else {
+				$($(this).find('.time__icon')[0]).removeClass('unrotate');
+				time_wrap.each(function(index) {
+					$(time_wrap[index]).addClass('time__closed')
+				});
+			}
+		})
+
     });
 })(jQuery);
--- routes/blog.js
+++ routes/blog.js
@@ -4,9 +4,84 @@ var models = require('../models');
 var moment = require('moment');
 var request = require('request');
 
+
+var getYearIndex = function(timeline, year) {
+	var yearIndex = -1;
+	for (var j in timeline) {
+		if (timeline[j].year == year) {
+			yearIndex = j;
+			break;
+		}
+	}
+	return yearIndex;
+}
+
+var pushPostToTimeline = function(timeline, year, month, monthNum, yearIndex, timelinePost) {
+	if (yearIndex == -1) {
+		var key = {}
+		key['year'] = year;
+		key['months'] = [];
+		key.months[monthNum] = {};
+		key.months[monthNum]['month'] = month;
+		key.months[monthNum]['posts'] = [];
+		key.months[monthNum]['posts'].push(timelinePost)
+		timeline.push(key)
+	} else if (timeline[yearIndex].months[monthNum] && timeline[yearIndex].months[monthNum]['posts']) {
+		timeline[yearIndex].months[monthNum]['posts'].push(timelinePost)
+	} else {
+		timeline[yearIndex].months[monthNum] = {}		
+		timeline[yearIndex].months[monthNum]['month'] = month;
+		timeline[yearIndex].months[monthNum]['posts'] = [];
+		timeline[yearIndex].months[monthNum]['posts'].push(timelinePost)
+	}
+
+	return timeline;
+}
+//This is the data structure of the posts
+// [ {
+//     2017: [ 
+//     	 6: {
+//        month: 'June',
+//        posts: []
+//	   	}]
+//     ]
+//   }
+// ]
+
+var buildTimeline = function(timelinePosts) {
+	var timeline = []
+	for (var i in timelinePosts) {
+		var year = moment(timelinePosts[i].createdAt).format('Y')
+		var month = moment(timelinePosts[i].createdAt).format('MMMM');
+		var monthNum = parseInt(moment(timelinePosts[i].createdAt).format('M'))
+		var yearIndex = getYearIndex(timeline, year);	
+		timeline = pushPostToTimeline(timeline, year, month, monthNum, yearIndex, timelinePosts[i]);
+	}
+	return timeline;
+}
+
+var denullifyTimeline = function(timeline) {
+	for (var i in timeline) {
+		var tempTimeline = []
+		for (var j in timeline[i].months) {
+			if (timeline[i].months[j] != null)
+				tempTimeline.push(timeline[i].months[j]);
+		}
+		timeline[i].months = tempTimeline;
+	}
+	return timeline;
+}
+
 router.get('/', function(req, res) {
+	var limit = 10;
+	var page = 0;
+	if (req.query.page) {
+		page = parseInt(req.query.page) - 1;
+	}
 	models.Post.findAndCountAll({
-		limit: 10,
+		limit: limit,
+		offset: page * limit,
+		distinct: true,
 		include: [{
 			model: models.Tag,
 			as: 'Tags'
@@ -16,6 +91,11 @@ router.get('/', function(req, res) {
 		}]
 	}).then(function(posts) {
 		posts = JSON.parse(JSON.stringify(posts))
+		var next = null;
+		var prev = null;
+		if (limit * (page + 1) < posts.count) next = '/blog?page=' + (page + 2);
+		if (page != 0 && page != 1) prev = '/blog?page=' + page;
+		else if (page != 0) prev = '/blog';
 		posts = posts.rows.map(function(post, index) {
 			post.time = moment(post.createdAt).format("MMM Do YYYY")
 			return post
@@ -28,26 +108,15 @@ router.get('/', function(req, res) {
 			}]
 		}).then(function(timelinePosts) {
 			timelinePosts = JSON.parse(JSON.stringify(timelinePosts))
-			var tlp = {} 
-			var years = []
-			for (var i in timelinePosts) {
-				var year = moment(timelinePosts[i].createdAt).format('Y')
-				var month = parseInt(moment(timelinePosts[i].createdAt).format('M'))
-				if (tlp[year] && tlp[year][month]) {
-					tlp[year][month].push(timelinePosts[i])
-				} else if (tlp[year] && !tlp[year][month]) {
-					tlp[year][month] = []
-					tlp[year][month].push(timelinePosts[i])
-				} else {
-					tlp[year] = [] 
-					tlp[year][month] = []
-					tlp[year][month].push(timelinePosts[i])
-				}
-			}
-			console.log('tlp2', tlp)
+			var timeline = buildTimeline(timelinePosts);
+			timeline = denullifyTimeline(timeline);
+
 			res.render('blog', {
 				posts: posts,
-				deserializedPosts: JSON.stringify(timelinePosts)
+				deserializedPosts: JSON.stringify(timelinePosts),
+				next: next,
+				prev: prev,
+				timeline: timeline
 			})
 		})
 	})
