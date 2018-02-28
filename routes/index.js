ea064ab83dfae66f1db2b141c81aae62624231c7
Alex <alexandercporter@gmail.com>
2018-02-28 09:58:03 -0700

    fixed up ping

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,37 @@ var router = express.Router();
 var locations = require('../data/locations');
 var posts = require('../data/posts.json');
 var ping = require('ping');
-var now = require("performance-now");
+var now = require('performance-now');
 
 // Get Time to Servers
-locations.forEach(function (host, i) {
-	var time;
-	var start = now();
-	
-	ping.sys.probe(host.hostname, function (isAlive) {
-		var msg = isAlive ? 'host ' + host.hostname + ' is alive' : 'host ' + host.hostname + ' is dead';
-		// console.log('message:', msg);
-	});
+locations.forEach(function(host, i) {
+  var time;
+  var regex = /(<)/;
+  var start = now();
+  var end;
+  var url = host.hostname.split('<')[0];
+  // console.log('its a url: ', url);
 
-	var end = now();
-	time = end - start;
+  ping.sys.probe(url, function(isAlive) {
+    var msg = isAlive ? 'host ' + url + ' is alive' : 'host ' + url + ' is dead';
+    end = now();
+    time = end - start;
+    // console.log('Time', time);
 
-	locations[i].time = time.toFixed(3);
-	// console.log('new location data with time:', locations[i]);
+    if (isAlive) {
+      locations[i].time = time.toFixed(3) + ' ms';
+    }
+  });
 });
 
 router.get('/', function(req, res) {
-	var post = posts[0];
-	res.render('home', {
-		locations: locations,
-		post: post,
-		the_title: 'PublicNTP',
-		relative_path: './',
-	})
-})
+  var post = posts[0];
+  res.render('home', {
+    locations: locations,
+    post: post,
+    the_title: 'PublicNTP',
+    relative_path: './',
+  });
+});
 
 module.exports = router;
