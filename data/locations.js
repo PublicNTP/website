3111f23c7326683d0c007b139096e8a9a4ec4539
Alex <alexandercporter@gmail.com>
2018-06-21 12:12:42 +0200

    removed korea, fixed ring animations, fixed blog posts and images

--- data/locations.js
+++ data/locations.js
@@ -4,10 +4,9 @@ module.exports = [
     country: 'KR',
     city: 'Seoul',
     className: 'coord--korea01',
-    hostname:
-      'stratum2-01.mia01.publicntp.org <br> stratum2-01.icn01.publicntp.org',
+    hostname: 'stratum2-01.icn01.publicntp.org',
     liveDate: '2018-05-06',
-    multiple: true,
+    multiple: false,
     IPv6: true
   },
   {
