e1c980a7013bcd145045855127abf3b2c8779b3e
Alex <alexandercporter@gmail.com>
2018-08-27 16:24:35 -0600

    changing body parse json

--- app.js
+++ app.js
@@ -43,9 +43,11 @@ app.set('view engine', 'hbs');
 
 app.enable('trust proxy');
 
-app.use(bodyParser.json(req, res, function (err) {
-  callback(err, req.body);
-}));
+app.use((req, res, next) => {
+  bodyParser.json(req, res, function (err) {
+    callback(err, req.body);
+  })
+});
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
