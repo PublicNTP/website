92a1e12682edb27489afbef3c15538ebe7c4d66d
Alex <alexandercporter@gmail.com>
2018-11-07 16:55:50 -0700

    removed extra index files

--- gulpfile.js
+++ gulpfile.js
@@ -235,29 +235,6 @@ gulp.task('routes', function () {
                     }
                 });
             })
-            // .then(function (pages) {
-            //     console.log('pages', pages);
-            //     let tempPath = path.join(__dirname, 'dist');
-
-            //     for (let i = 0; i < pages.length; i++) {
-            //         let route = routes[i];
-            //         console.log('route', route);
-            //         if (route == '/' && argv.env == 'production') route = '/index.html';
-            //         if (route == '/index-dev' && argv.env == 'dev') route = '/index.html';
-            //         if (route == '/index-staging' && argv.env == 'staging') route = '/index.html';
-
-
-            //         var routeDir = tempPath + route.substring(0, route.lastIndexOf('/'));
-            //         if (fs.existsSync(routeDir)) {
-            //             createFile(route, pages[i]);
-            //         } else {
-            //             mkdirp(routeDir, function (mkdirErr) {
-            //                 if (mkdirErr) console.log('mkdirpErr', mkdirErr);
-            //                 else createFile(route, pages[i]);
-            //             });
-            //         }
-            //     }
-            // })
             .catch(function (err) {
                 console.log('err', err);
                 console.log('err with');
